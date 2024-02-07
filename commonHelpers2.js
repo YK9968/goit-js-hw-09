db00362f6a75af5824a23523c958a8f9c424d2d5
YK9968 <144912821+YK9968@users.noreply.github.com>
2024-02-07 11:48:04 +0000

    Deploying to gh-pages from @ YK9968/goit-js-hw-09@2bd2cff4cbca6a4ce1beb7bfc9c660dc657533eb 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-c170f314.js";console.log("form");
+import"./assets/styles-c170f314.js";const a="feedback-form-state",t=document.querySelector(".feedback-form"),o=t.elements.email,l=t.elements.message;t.addEventListener("input",s);function s(e){e.preventDefault();const r={email:`${o.value.trim()}`,message:`${l.value.trim()}`};localStorage.setItem(a,JSON.stringify(r))}const n=localStorage.getItem(a);if(n!==""){const e=JSON.parse(n);o.value=e.email,l.value=e.message}t.addEventListener("submit",m);function m(e){e.preventDefault(),!(o.value===""||l.value==="")&&(console.log(JSON.parse(localStorage.getItem(a))),localStorage.removeItem(a),t.reset())}
 //# sourceMappingURL=commonHelpers2.js.map
